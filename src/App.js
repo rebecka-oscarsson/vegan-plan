644faeb212097d482ab154926118997ee714d61f
Antonia Pettersson <antoniapettersson@Antonias-MacBook-Pro.local>
2021-11-04 21:32:45 +0100

    navbar and Routuer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
-import Start from './components/Start';
-import Navbar from './components/Navbar';
-import Recipes from './components/Recipes';
-import Filter from './components/Filter';
-import Footer from './components/Footer';
-import Search from './components/Search';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import { Switch } from "react-router";
+
+import Start from "./components/Start";
+import Navbar from "./components/Navbar";
+import AboutUs from "./components/AboutUs";
+import Recipes from "./components/Recipes";
+import Filter from "./components/Filter";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
 
 const App = () => {
   const fetchData = async () => {
-    const response = await fetch('http://localhost:3001/recipes');
+    const response = await fetch("http://localhost:3001/recipes");
     const data = await response.json();
-    console.log('myData', data);
+    console.log("myData", data);
   };
 
   useEffect(() => {
@@ -19,14 +23,25 @@ const App = () => {
 
   return (
     <>
-      <Navbar />
-      <Start />
-      <Search />
-      <Filter />
-      <Recipes />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Switch>
+          <div className="pages">
+            <Route exact path="/" component={Start} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/recipes" component={Recipes} />
+          </div>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 };
 
 export default App;
+
+// <Start />
+// <Search />
+// <Filter />
+// <Recipes />
+// <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,72 @@
-const Navbar = () => {
-  return <h1>Navbar</h1>;
-};
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export default Navbar;
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            CodeBucks
+            <i className="fas fa-code"></i>
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                about
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/recepies"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                recepies
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/minasidor"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Mina sidor
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
