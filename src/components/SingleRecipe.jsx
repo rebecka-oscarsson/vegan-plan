f2f3189c0cf83e0d2b5495b63b39b892996ed195
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-11-09 13:25:40 +0100

    added fetch for instructions

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -1,4 +1,23 @@
+import fetchData from './fetchData';
+import { useEffect, useState } from 'react';
+const apiKey = process.env.REACT_APP_API_KEY;
+
 const SingleRecipe = (props) => {
+  console.log(props)
+  const [instructions, setInstructions] = useState();
+
+  useEffect(() => {
+
+    fetchData(
+      'https://api.spoonacular.com/recipes/' +
+      props.oneRecipe[0].id +
+        '/information?apiKey=' +
+        apiKey
+    ).then((data) => {
+      setInstructions(data.instructions)
+    });
+
+  }, []);
   return (
     <>
       <h2>{props.oneRecipe[0].title}</h2>
@@ -13,6 +32,7 @@ const SingleRecipe = (props) => {
         </a>
       </p>
       <img src={props.oneRecipe[0].image} />
+      {instructions}
     </>
   );
 };
