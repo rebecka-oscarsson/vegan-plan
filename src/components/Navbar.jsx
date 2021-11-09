c2ff3e68a07ac38543ce7e06df2691d52a0082ed
Antonia Pettersson <antoniapettersson@iMac-som-tillhor-Antonia.local>
2021-11-09 14:15:03 +0100

    new navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,89 @@
-const Navbar = () => {
-  return <h1>Navbar</h1>;
-};
+import React, { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
-export default Navbar;
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+          <img
+              src={"./vegan-plan-logo.png"}
+              width={130}
+              alt="Vegan Plan logo"
+            />
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/recept"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                recepies
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/mina-sidor"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                 <button className="btn-myPage">
+               Mina sidor
+              </button>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/registrate"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                registrate
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
