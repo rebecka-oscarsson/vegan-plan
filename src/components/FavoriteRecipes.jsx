0deca0c372ed5b36569b4785834c2466d6d85da6
Joanna Beyene <joanna.beyene@gmail.com>
2021-11-11 12:23:17 +0100

    created weeklyMenu and fetch to FavoriteRecipes component

--- src/components/FavoriteRecipes.jsx
+++ src/components/FavoriteRecipes.jsx
@@ -1,66 +1,35 @@
-import React from 'react';
-import beetrootsoup  from '../img/beetrootsoup.jpg'
-import ricepaperroll  from '../img/ricepaperroll.jpg'
-import ricepudding  from '../img/ricepudding.jpg'
-import toast  from '../img/toast.jpg'
-import veggieburger  from '../img/veggieburger.jpg'
-import veggiepizza  from '../img/veggiepizza.jpg'
-import tempehskewers  from '../img/tempehskewers.jpg'
+import { useState, useEffect } from "react";
 
 const FavoriteRecipes = () => {
+    const [menuData, setMenuData] = useState([]);
+
+    const fetchMenuData= async ()=>{
+        const res = await fetch('http://localhost:3001/weeklyMenu');
+        const data = await res.json();
+        console.log("Weekly Menu:", data);
+        return data;
+    }
+    useEffect(() => {
+        const getMenuData = async () => {
+            const menu = await fetchMenuData();
+            setMenuData(menu);
+          };
+          getMenuData();
+      }, []);
+
   return (
     <>
     <h2>Veckans recept</h2>
     <div className="Favorite-Recipes-Grid">
-        <div className="Favorite-Recipes-Card">
-            <p>MÅNDAG</p>
-            <figure className="Recipes">
-            <img src={beetrootsoup}/>
-            <figcaption>Beetroot Soup with Sour Cream Swirl and Wholegrain Bread</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card">
-            <p>TISDAG</p>
-            <figure className="Recipes">
-            <img src={toast}/>
-            <figcaption>Toasted Sourdough Bread with Toppings</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card"> 
-            <p>ONSDAG</p>
-            <figure className="Recipes">
-            <img src={veggieburger}/>
-            <figcaption>The Ultimate Veggie Burger</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card">
-            <p>TORSDAG</p>
-            <figure className="Recipes">
-            <img src={veggiepizza}/>
-            <figcaption>Veggie Pizza</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card">
-            <p>FREDAG</p>
-            <figure className="Recipes">
-            <img src={ricepaperroll}/>
-            <figcaption>Rainbow Rice Paper Rolls with Peanut Sauce</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card">
-            <p>LÖRDAG</p>
-            <figure className="Recipes">
-            <img src={tempehskewers}/>
-            <figcaption>Spicy Tempeh Skewers with Peanut Sauce</figcaption>
-            </figure>
-        </div>
-        <div className="Favorite-Recipes-Card">
-            <p>SÖNDAG</p>
-            <figure className="Recipes">
-            <img src={ricepudding}/>
-            <figcaption>Ricepudding with Fruits and Berries Topping</figcaption>
-            </figure>
-        </div>
+            {menuData.map((weeklyMenu)=> {
+                return <div className="Favorite-Recipes-Card" key={weeklyMenu.id}>
+                    <p>{weeklyMenu.day}</p>
+                    <figure className="Recipes">
+                    <img src={weeklyMenu.image}/>
+                    <figcaption>{weeklyMenu.title}</figcaption>
+                </figure>
+                </div>
+            })}
     </div>
     </>
   )
